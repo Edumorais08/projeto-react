26874a3d335a3bb7b51f89718bb269e13971a91b
pLopess <pedro.lopess.2001@gmail.com>
2024-12-01 18:15:48 -0300

    atualização navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,31 @@
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
-
   const location = useLocation(); 
 
   const isHomePage = location.pathname === "/";
 
   return (
     <div>
-        <nav className="header">
-            <h2 >
-                <Link to="/" className="page-title">OrcMDB</Link>
-            </h2>
+      <nav className="header">
+        <h2>
+          <Link to="/" className="page-title">OrcMDB</Link>
+        </h2>
 
-            
-            {isHomePage && (
+        <div className="header-links">
+          {isHomePage && (
             <>
               <a href="#nos-cinemas" className="header-link">Nos Cinemas</a>
               <a href="#em-breve" className="header-link">Em Breve</a>
+              <a href="#favoritos" className="header-link">Favoritos</a>
             </>
-            )}
-        </nav>
+          )}
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
