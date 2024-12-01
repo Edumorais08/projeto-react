fd86faf7bfe6a54376176388c0467ed09c65c1ee
pLopess <pedro.lopess.2001@gmail.com>
2024-12-01 18:15:17 -0300

    botao de favoritar

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,44 +1,78 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useFavorites } from "../../context/FavoriteContext";
 
-import noImg from "../../assets/no-img.png"
-import "./MovieCard.css"
+import noImg from "../../assets/no-img.png";
+import "./MovieCard.css";
 
 const MovieCard = (props) => {
+  const { addFavorite, removeFavorite, favorites } = useFavorites();
 
-    const date = props.release_date;
-    const date_br = date[8] + date[9] + '/' + date[5] + date[6] + '/' + date[0] + date [1] + date [2] + date[3];
+  const date = props.release_date;
+  const date_br =
+    date[8] +
+    date[9] +
+    "/" +
+    date[5] +
+    date[6] +
+    "/" +
+    date[0] +
+    date[1] +
+    date[2] +
+    date[3];
 
-  return (
-    <Link to={`/movie/${props.id}`} className="movie-link">
-        <li className="movie-card" >
-        {props.poster_path ? (
-        <div className="movie-poster">
-          <img
-            className="poster-img"
-            src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
-            alt={`poster de '${props.title}'`}
-          />
-        </div>
-          ) : (
-        <div className="poster-no-img">
-          <img src={noImg} className="no-img" alt="sem imagem"></img>
-        </div>
-            
-          )}
-        
-            <p className="movie-title"> {props.title} </p>
-            <p className="movie-date"> Lançamento: {date_br} </p>
-        </li>
-    </Link>
-  )
-}
+  const isFavorite = favorites.some((fav) => fav.id === props.id);
 
-MovieCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    poster_path: PropTypes.string,
-    release_date: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      removeFavorite(props.id);
+    } else {
+      addFavorite({
+        id: props.id,
+        title: props.title,
+        poster_path: props.poster_path,
+        release_date: props.release_date,
+      });
+    }
   };
 
-export default MovieCard
+  return (
+    <li className="movie-card">
+      <Link to={`/movie/${props.id}`} className="movie-link">
+        {props.poster_path ? (
+          <div className="movie-poster">
+            <img
+              className="poster-img"
+              src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+              alt={`poster de '${props.title}'`}
+            />
+          </div>
+        ) : (
+          <div className="poster-no-img">
+            <img src={noImg} className="no-img" alt="sem imagem" />
+          </div>
+        )}
+      </Link>
+  
+      <div className="movie-info">
+        <p className="movie-title">{props.title}</p>
+        <p className="movie-date">Lançamento: {date_br}</p>
+        <button
+          onClick={handleFavoriteClick}
+          className={`favorite-btn ${isFavorite ? "is-favorite" : ""}`}
+        >
+          {isFavorite ? "Remover dos Favoritos" : "Adicionar aos Favoritos"}
+        </button>
+      </div>
+    </li>
+  );  
+};
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster_path: PropTypes.string,
+  release_date: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
+
+export default MovieCard;
