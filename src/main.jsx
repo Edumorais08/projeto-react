4b2e7b824bb2a030bdda4dd15430cacc9a45182f
pLopess <pedro.lopess.2001@gmail.com>
2024-12-01 18:16:09 -0300

    atualização rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
-import './index.css'
+import './index.css';
 
-import App from './App.jsx'
+import App from './App.jsx';
 import Home from './pages/Home/Home.jsx';
 import Movie from './pages/Movie/Movie.jsx';
-import UserManager from "./pages/UserPage.jsx"
-
-
+import UserManager from "./pages/UserPage.jsx";
+import Favorites from "./pages/Favorites/Favorties.jsx";
+import { FavoriteProvider } from './context/FavoriteContext'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/movie/:id' element={<Movie />} />
-          <Route path='/login' element={<UserManager />} />
-        </Route>
-      </Routes>
-    </BrowserRouter> 
+    <FavoriteProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<App />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/movie/:id' element={<Movie />} />
+            <Route path='/login' element={<UserManager />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </FavoriteProvider>
   </StrictMode>
 );
